e496e29d2881d2d901deccf30db13c55c7e1de60
rupak <you@example.com>
2021-04-26 09:59:30 +0530

    Added AOP Aspect

--- src/main/java/com/example/home/MongoDB/Spring/Boot/MongoDbSpringBootApplication.java
+++ src/main/java/com/example/home/MongoDB/Spring/Boot/MongoDbSpringBootApplication.java
@@ -1,8 +1,5 @@
 package com.example.home.MongoDB.Spring.Boot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/example/home/MongoDB/Spring/Boot/aspect/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.example.home.MongoDB.Spring.Boot.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("execution (* com.example.home.MongoDB.Spring.Boot.*.*(..))")
+    public void aroundAdviceForAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        logger.info("Before advise method execution :" + proceedingJoinPoint.getSignature().getName());
+        proceedingJoinPoint.proceed();
+        logger.info("After advise method execution : " + proceedingJoinPoint.getSignature().getName());
+    }
+}
